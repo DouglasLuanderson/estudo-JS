fcb4a5fe8ebff303ed8bdc005cab22db5e080190
VaiNaWeb <vainaweb@gmail.com>
2019-07-15 14:17:10 -0300

    informações

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 window.alert('Hello word')
 window.confirm('lets go JS')
 window.prompt('Inserir seu nome')
+
+
+
+
+
+
+//Anotações 
+
+/*tipos primitivos e variáveis
+
+    - number
+        Nan (error número não identificado)
+        Infinity (Infinito é exibido quando um número excede o limite superior dos números de ponto flutuante , que é 1.797693134862315E + 308.)
+    - string ("" , '')
+    - boolean (true, false)
+    - null (null)
+        - undefined (Uma variável que não teve um valor atribuído é do tipo undefined)
+    - object ({} , [])
+        array 
+    - function (function(){} Uma função JavaScript é um bloco de código projetado para executar uma tarefa específica.)
+*/ 
